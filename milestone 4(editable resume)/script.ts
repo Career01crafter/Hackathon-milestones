129a75e2f290a2fc57e6ad459de78398ac58c378
Career01Crafter <163905451+Career01crafter@users.noreply.github.com>
2024-09-15 15:22:16 +0500

    Add files via upload

---
+++ milestone 4(editable resume)/script.ts
@@ -0,0 +1,41 @@
+//Code for the resume to display!
+const form = document.getElementById('form') as HTMLFormElement
+const resumeDisplayElement = document.getElementById('displayed-resume') as HTMLDivElement
+
+//Form Submission!
+form.addEventListener('submit', (event: Event) => {
+    event.preventDefault();       //prevention of page reload
+
+    //input values
+    const name = (document.getElementById('name') as HTMLInputElement).value
+    const contactno = (document.getElementById('contact-no') as HTMLInputElement).value
+    const email = (document.getElementById('email') as HTMLInputElement).value
+    const education = (document.getElementById('education') as HTMLInputElement).value
+    const experience = (document.getElementById('experience') as HTMLInputElement).value
+    const skills = (document.getElementById('skills') as HTMLInputElement).value
+
+    //Dynamically generated resume
+    const HtmlResume = `
+    <h2><b>Editable Resume</b></h2>
+    <h3>Personal Information</h3>
+    <p><b>Name:</b><span contenteditable="true">${name}</span></p>
+    <p><b>Contactno:</b><span contenteditable="true">${contactno}</span></p>
+    <p><b>Email:</b><span contenteditable="true">${email}</span></p>
+
+    <h3>Education</h3>
+    <p contenteditable="true">${education}</p>
+
+    <h3>Experience</h3>
+    <p contenteditable="true">${experience}</p>
+
+    <h3>Skills</h3>
+    <p contenteditable="true">${skills}</p>
+    `;
+
+    //Final display
+    if(resumeDisplayElement) {
+        resumeDisplayElement.innerHTML = HtmlResume
+    } else {
+        console.error('Something was incorrect!');
+    }
+});
